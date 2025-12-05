1cca12d9e835c7a07c00ac8c9b76e082196845cb
RexTor78 <progrextor@gmail.com>
2025-12-05 15:19:23 +0100

    fix: change code to insert header how custom heder

--- js/header.js
+++ js/header.js
@@ -1,49 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
+class myHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header class="header-mariria" id="headerMenu">
+        <div class="logoMenu">
+            <a href="../index.html">
+                <img src="../assets/img/logo.png" alt="Logo MAR I RÍA" class="logoImageSize">
+            </a>
+        </div>
 
-  const header = `
-        <header class="header-mariria" id="headerMenu">
-            <div class="logoMenu">
-                <a href="../index.html">
-                    <img src="/img/logo.png" alt="Logo MAR I RÍA" class="logoImageSize">
-                </a>
-            </div>
+        <div class="hamburgerMenu" id="hamburgerBtn">
+            <img id="hamburgerIcon" 
+                 src="../assets/img/NavigationImg/menuClosed.png" 
+                 alt="Abrir menú" 
+                 class="hamburgerImage">
+        </div>
 
-            <div class="hamburgerMenu" id="hamburgerBtn">
-                <img id="hamburgerIcon" 
-                     src="/img/NavigationImg/menuOpen.png" 
-                     alt="Abrir menú" 
-                     class="hamburgerImage">
-            </div>
-
-            <nav class="navbar hidden" id="menu">
-                <ul>
-                    <li><a href="../index.html">Inicio</a></li>
-                    <li><a href="../productos.html">Productos</a></li>
-                    <li><a href="../contacto.html">Contacto</a></li>
-                    <li><a href="../sobre-nosotros.html">Sobre Nosotros</a></li>
-                    <li class="cesta-item"><a href="../cesta.html">Cesta</a></li>
-                </ul>
-            </nav>
-        </header>
+        <nav class="navbar hidden" id="menu">
+            <ul>
+                <li><a href="../index.html">Inicio</a></li>
+                <li><a href="../productos.html">Productos</a></li>
+                <li><a href="../contacto.html">Contacto</a></li>
+                <li><a href="../sobre-nosotros.html">Sobre Nosotros</a></li>
+                <li class="cesta-item"><a href="../cesta.html">Cesta</a></li>
+            </ul>
+        </nav>
+      </header>
     `;
 
-  document.body.insertAdjacentHTML("afterbegin", header);
+    const menu = this.querySelector("#menu");
+    const btn = this.querySelector("#hamburgerBtn");
+    const icon = this.querySelector("#hamburgerIcon");
 
-  const menu = document.getElementById("menu");
-  const btn = document.getElementById("hamburgerBtn");
-  const icon = document.getElementById("hamburgerIcon");
+    let abierto = false;
 
-  let abierto = false;
+    btn.addEventListener("click", () => {
+      abierto = !abierto;
+      menu.classList.toggle("hidden");
 
-  btn.addEventListener("click", () => {
-    abierto = !abierto;
-    menu.classList.toggle("hidden");
+      icon.src = abierto
+        ? "../assets/img/NavigationImg/menuOpen.png"
+        : "../assets/img/NavigationImg/menuClosed.png";
+    });
+  }
+}
 
-    if (abierto) {
-      icon.src = "/img/NavigationImg/menuOpen.png";
-    } else {
-      icon.src = "/img/NavigationImg/menuClosed.png";
-    }
-  });
-
-});
+customElements.define("my-custom-header", myHeader);
