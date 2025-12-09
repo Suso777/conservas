fce8b67ac40b1e4dc01c7017f8935044818975ef
gzifoto-spec <gzifoto@gmail.com>
2025-12-09 13:03:11 +0100

    feature: uploading 1st footer

---
+++ assets/js/footer.js
@@ -0,0 +1,47 @@
+class myFooter extends HTMLElement {
+connectedCallback() {
+    this.innerHTML = `
+    <footer class="header-mariria" id="headerMenu">
+        <div class="logoMenu">
+            <a href="../index.html">
+                <img src="../assets/img/logo.png" alt="Logo MAR I RÍA" class="logoImageSize">
+            </a>
+        </div>
+
+        <div class="hamburgerMenu" id="hamburgerBtn">
+            <img id="hamburgerIcon" 
+                src="../assets/img/NavigationImg/menuClosed.png" 
+                alt="Abrir menú" 
+                class="hamburgerImage">
+        </div>
+
+        <nav class="navbar hidden" id="menu">
+            <ul>
+                <li><a href="../index.html">Inicio</a></li>
+                <li><a href="../productos.html">Productos</a></li>
+                <li><a href="../contacto.html">Contacto</a></li>
+                <li><a href="../sobre-nosotros.html">Sobre Nosotros</a></li>
+                <li class="cesta-item"><a href="../cesta.html">Cesta</a></li>
+            </ul>
+        </nav>
+    </header>
+    `;
+
+    const menu = this.querySelector("#menu");
+    const btn = this.querySelector("#hamburgerBtn");
+    const icon = this.querySelector("#hamburgerIcon");
+
+    let abierto = false;
+
+    btn.addEventListener("click", () => {
+        abierto = !abierto;
+        menu.classList.toggle("hidden");
+
+        icon.src = abierto
+        ? "../assets/img/NavigationImg/menuOpen.png"
+        : "../assets/img/NavigationImg/menuClosed.png";
+    });
+    }
+}
+
+customElements.define("my-custom-footer", myFooter);
