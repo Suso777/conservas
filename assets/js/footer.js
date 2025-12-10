4021993db58168dc9669645b7f4540bbec7f80d4
gzifoto-spec <gzifoto@gmail.com>
2025-12-10 10:35:19 +0100

    feat: uploading most likely definitice archives footer.html, footer.css and footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -3,28 +3,37 @@
 class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+        
         <footer class="footer-mariria" id="footerMenu">
-        <a href="https://instagram.com" target="_blank" rel="noopener" class="social-link">
-            <i class="fa-brands fa-instagram"></i>
-        </a>
+        
+        <div class="grid">
+            
+            <div class="one">
 
-        <a href="https://facebook.com" target="_blank" rel="noopener" class="social-link">
-            <i class="fa-brands fa-facebook"></i>
-        </a>
+                <a href="https://instagram.com" target="_blank" rel="noopener" class="social-link">
+                    <i class="fa-brands fa-instagram"></i></a>
+                
+                <a href="https://facebook.com" target="_blank" rel="noopener" class="social-link">
+                    <i class="fa-brands fa-facebook"></i></a>
 
-        <a href="https://x.com" target="_blank" rel="noopener" class="social-link">
-            <i class="fa-brands fa-x-twitter"></i>
-        </a>
+                <a href="https://x.com" target="_blank" rel="noopener" class="social-link">
+                    <i class="fa-brands fa-x-twitter"></i></a>
 
-        <a href="https://discord.com" target="_blank" rel="noopener" class="social-link">
-            <i class="fa-brands fa-discord"></i>
-        </a>
-        <p></p>
-        <div class="DD">
-            copyright 2025 Dream Developers
+                <a href="https://discord.com" target="_blank" rel="noopener" class="social-link">
+                    <i class="fa-brands fa-discord"></i></a>
+
+            </div>
+    
+            <div class="two">
+                <hr>
+            </div>
+
+            <div class="three">
+                <div class="dreamDev">
+                    copyright 2025 <img src="../assets/img/shared/icons/developers-icon-1.png" width="6%" alt="logo Dream Developers" title="dreamdevelopers.com">
+                </div>
+            <div>
         </div>
-
-
 
         </footer>
     `;
