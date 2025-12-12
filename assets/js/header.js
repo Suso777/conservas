16a20e8335e1196e73b36b392ccfd863dd2c2b74
RexTor78 <progrextor@gmail.com>
2025-12-12 09:28:35 +0100

    fix: change links for github pages

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,14 +3,14 @@ class myHeader extends HTMLElement {
     this.innerHTML = `
       <header class="header-mariria" id="headerMenu">
         <div class="logoMenu">
-            <a href="../index.html">
-                <img src="../assets/img/logo.png" alt="Logo MAR I RÍA" class="logoImageSize">
+            <a href="index.html">
+                <img src="assets/img/logo.png" alt="Logo MAR I RÍA" class="logoImageSize">
             </a>
         </div>
 
         <div class="hamburgerMenu" id="hamburgerBtn">
             <img id="hamburgerIcon" 
-                 src="../assets/img/NavigationImg/menuClosed.png" 
+                 src="assets/img/NavigationImg/menuClosed.png" 
                  alt="Abrir menú" 
                  class="hamburgerImage">
         </div>
@@ -39,8 +39,8 @@ class myHeader extends HTMLElement {
       menu.classList.toggle("hidden");
 
       icon.src = abierto
-        ? "../assets/img/NavigationImg/menuOpen.png"
-        : "../assets/img/NavigationImg/menuClosed.png";
+        ? "assets/img/NavigationImg/menuOpen.png"
+        : "assets/img/NavigationImg/menuClosed.png";
     });
   }
 }
