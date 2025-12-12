0ca8cc5311347067748c339179bc962c515617b5
RexTor78 <progrextor@gmail.com>
2025-12-12 09:21:19 +0100

    fix: repair links for github pages

--- assets/js/header.js
+++ assets/js/header.js
@@ -17,11 +17,11 @@ class myHeader extends HTMLElement {
 
         <nav class="navbar hidden" id="menu">
             <ul>
-                <li><a href="../index.html">Inicio</a></li>
-                <li><a href="../html/aboutUs.html">Conoce Mar i Ría</a></li>
-                <li><a href="../html/developers.html">Nosotros</a></li>
-                <li><a href="../html/contact.html">Contacto</a></li>
-                <li><a href="../html/saleProducts.html">Productos</a></li>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="html/aboutUs.html">Conoce Mar i Ría</a></li>
+                <li><a href="html/developers.html">Nosotros</a></li>
+                <li><a href="html/contact.html">Contacto</a></li>
+                <li><a href="html/saleProducts.html">Productos</a></li>
                 <li class="cesta-item"><a href="../html/basket.html">Cesta</a></li>
             </ul>
         </nav>
